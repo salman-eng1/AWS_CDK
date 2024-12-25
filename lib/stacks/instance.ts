2e4ff76f003285588f059526ac807cd999c643f5
Salman Ali <salman-eng1>
2024-12-26 02:59:33 +0300

    loadbalancer in progress

--- lib/stacks/instance.ts
+++ lib/stacks/instance.ts
@@ -1,5 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import { InstanceTarget } from 'aws-cdk-lib/aws-elasticloadbalancingv2-targets';
+
 import { Construct } from 'constructs';
 import { readFileSync } from 'fs';
 
@@ -42,7 +45,6 @@ export class InstanceStack extends cdk.Stack {
     // User Data Scripts
     const userDataScript1 = readFileSync('./lib/scripts/user-data1.sh', 'utf-8');
     const userDataScript2 = readFileSync('./lib/scripts/user-data2.sh', 'utf-8');
-    const userDataScript3 = readFileSync('./lib/scripts/user-data3.sh', 'utf-8');
 
     // Create EC2 Instances
     const instance1 = new ec2.Instance(this, 'Instance1_CDK', {
@@ -77,25 +79,46 @@ export class InstanceStack extends cdk.Stack {
       userData: ec2.UserData.custom(userDataScript2),
     });
 
-    const instance3 = new ec2.Instance(this, 'Instance3_CDK', {
-      instanceName: 'instance3_cdk',
-      blockDevices: [rootVolume],
+    const alb = new elbv2.ApplicationLoadBalancer(this, 'ALB', {
       vpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
-      machineImage: new ec2.AmazonLinux2023ImageSsmParameter({
-        kernel: ec2.AmazonLinux2023Kernel.KERNEL_6_1,
-      }),
-      keyPair: ec2.KeyPair.fromKeyPairName(this, 'ExistingKeyPair3', 'cdk_ssh'),
-      vpcSubnets: { 
-        subnets: vpc.publicSubnets,
+      internetFacing: true,
+      vpcSubnets:{
+        availabilityZones: vpc.availabilityZones,
+        subnetType: ec2.SubnetType.PUBLIC,
       },
-      securityGroup: securityGroup,
-      userData: ec2.UserData.custom(userDataScript3),
+      securityGroup: securityGroup
+    });
+
+    // Create Target Group
+    const instanceTargetGroup = new elbv2.ApplicationTargetGroup(this, 'InstanceTargetGroup', {
+      vpc: vpc,
+      port: 80,
+      // protocol: elbv2.ApplicationProtocol.HTTP,
+      targetGroupName: 'instanceTargetGroup',
+      targetType: elbv2.TargetType.INSTANCE,
+      healthCheck: {
+        path: '/',
+        port: '80',
+        protocol: elbv2.Protocol.HTTP
+      }
+    });
+
+    // Add EC2 instances as targets using InstanceTarget
+    instanceTargetGroup.addTarget(new InstanceTarget(instance1));
+    instanceTargetGroup.addTarget(new InstanceTarget(instance2));
+
+    // Create Listener for the ALB
+    const listener = alb.addListener('Listener', {
+      port: 80,
+    });
+
+    // Add Target Group to Listener
+    listener.addTargetGroups('TargetGroup1', {
+      targetGroups: [instanceTargetGroup],
     });
 
     // Tags for EC2 Instances
     cdk.Tags.of(instance1).add('Name', 'CDK_pr1');
     cdk.Tags.of(instance2).add('Name', 'CDK_pr2');
-    cdk.Tags.of(instance3).add('Name', 'public');
   }
 }
